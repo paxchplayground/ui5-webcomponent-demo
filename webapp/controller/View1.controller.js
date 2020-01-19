28f792eadc8b67519f01cd1184e419ec80c834c2
Till Weisser <till.weisser@pax.ch>
2020-01-19 22:14:19 +0000

    Add webcomponents

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"demo/ui5-webcomponent-demo/lib/game-of-life"
+], function (Controller, GameOfLife) {
 	"use strict";
 
 	return Controller.extend("demo.ui5-webcomponent-demo.controller.View1", {
